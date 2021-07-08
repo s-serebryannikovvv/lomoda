30c5238b5fa3b654aeb378232a3f84674839a1ac
Sergey <s.serebryannikovvv@mail.ru>
2021-07-08 23:09:49 +0300

    createCart

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const headerCityButton = document.querySelector('.header__city-button');
 
+let hash = location.hash;
+
 if (localStorage.getItem('lomoda-location')) {
 	headerCityButton.textContent = localStorage.getItem('lomoda-location')
 }
@@ -73,7 +75,7 @@ cartOverlay.addEventListener('click', event => {
 
 //get data (запрос базы данных)
 
-const getData = async () => {//Универсальная функция для запроса с сервера
+const getData = async () => {//Универсальная функция для запроса (получение данных) с сервера
 	const data = await fetch('db.json') // await заставляет ожидать и не выполняет присваивание пока fetch не вернет ответ. без await получим промис
 	if (data.ok) {
 		return data.json();
@@ -92,6 +94,61 @@ const getGoods = callback => {//обработка данных
 		})
 }
 
-getGoods((data) => {
-	console.warn(data);
-})
+
+//
+
+try {
+	const goodsList = document.querySelector('.goods__list');
+
+	if (!goodsList) {
+		throw 'This is not a goods page!'
+	}
+
+	const createCart = ({ id, preview, cost, brand, name, sizes }) => {//деструктуризация 
+		const li = document.createElement('li');//создаём элемент li 
+
+
+		li.classList.add('goods__item'); // добавляем новому элементу класс
+
+		//выводим созданный элемент на страницу
+		li.innerHTML = ` 
+			<article class="good">
+				<a class="good__link-img" href="card-good.html#id${id}">
+					<img class="good__img" src="goods-image/${preview}" alt="" />
+				</a>
+				<div class="good__description">
+					<p class="good__price">${cost} &#8381;</p>
+					<h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+					${sizes ?
+				`<p class="good__sizes">Размеры (RUS):<span class="good__sizes-list">${sizes.join(' ')}</span></p>` :
+				''
+			}
+					
+					<a class="good__link" href="card-good.html#id56454">Подробнее</a>
+				</div>
+			</article>
+		`
+
+		return li
+	};
+
+	const renderGoodsList = data => {
+		goodsList.textContent = ''
+
+		// for (let i = 0; i < data.length; i++) {
+		// 	console.log(data[i]);
+		// }
+		// for (const item of data) {
+		// 	console.log(item);
+		// }
+		data.forEach((item) => {
+			const card = createCart(item);
+			goodsList.append(card);
+		})
+
+	};
+	getGoods(renderGoodsList)
+
+} catch (err) {
+	console.warn(err)
+}
